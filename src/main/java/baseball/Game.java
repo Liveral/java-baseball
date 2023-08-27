9e3855d2edcc384de2523875491b3908154ae401
Yeonwoo <yoanwoo0227@gmail.com>
2023-08-27 17:42:03 +0900

    feat : 스트라이크인지 체크하고 아니면 볼 체크 함수를 불러오는 함수와 볼인지 체크하는 함수
    getStrikeAndBall() 메소드에서는 컴퓨터 숫자 3자리와 사용자 숫자 3자리를 자릿수마다 각각 비교하여 같은 자리에 같은 숫자가 있는지 확인하여 있으면 strike 변수를 1 증가시키고, 없으면 getBall()함수를 불러온다.
    getBall()함수는 인덱스 값을 매개변수로 받아서 해당 인덱스 순서에 있는 사용자의 숫자가 컴퓨터의 숫자 리스트에 포함되는지 contains()를 이용해 확인하고, 있으면 ball을 1 증가시킨다.

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,48 @@
+package baseball;
+
+public class Game {
+
+    private static Computer computerNumbers = new Computer();
+    private static User userNumbers = new User();
+    private static int strike;
+    private static int ball;
+
+    public void playGame() {
+        computerNumbers.setComputerNumber();
+
+        while (true) {
+            strike = 0;
+            ball = 0;
+
+            userNumbers.setUserNumber();
+            getStrikeAndBall();
+            printResult();
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
+    }
+
+    public static void getStrikeAndBall() { //스트라이크인지 체크하고, 아니면 볼인지 체크한다.
+        for (int i = 0; i < 3; i++) {
+            //컴퓨터와 사용자의 3자리 숫자를 각 자릿수 별로 같은 자리에 같은 숫자가 있는지 확인하고, 있으면 스트라이크 없으면 볼 체크 함수를 불러온다.
+            if (computerNumbers.getComputerNumberIdx(i) == userNumbers.getUserNumberIdx(i)) {
+                strike++;
+            } else {
+                getBall(i);
+            }
+        }
+    }
+
+    public static void getBall(int idx) {
+        //컴퓨터 숫자 리스트에 매개변수로 받은 인덱스번째에 있는 사용자의 숫자가 포함되어 있으면 볼
+        if (computerNumbers.getComputerNumber().contains(userNumbers.getUserNumberIdx(idx))) {
+            ball++;
+        }
+    }
+
+
+}
+
