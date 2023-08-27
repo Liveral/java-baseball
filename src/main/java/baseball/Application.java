e7119f9552295645732f2992c2c0190f4aee0f5a
Yeonwoo <yoanwoo0227@gmail.com>
2023-08-27 17:45:57 +0900

    feat : 새로운 게임을 시작할지 끝낼지 입력에 따라 return값을 달리하는 boolean 함수
    입력된 숫자가 1이면 false를 return하고 2이면 true를 return한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+    }
+
+    public static boolean reStartOrEnd() {
+        int inputNumber = Integer.parseInt(Console.readLine());
+
+        return inputNumber == Constants.END_NUMBER;
     }
 }
