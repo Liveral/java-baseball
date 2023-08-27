60deecf617be662f0305e1c1e5d182dcf0f46118
Yeonwoo <yoanwoo0227@gmail.com>
2023-08-27 16:26:32 +0900

    feat : 사용자의 숫자를 설정해주는 setter함수
    입력받은 문자열을 3번 끊어서 user문자열에 하나씩 저장해준다.
    그리고 완성된 user리스트를 userNumber에 setting해준다.

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public abstract class User {
+    private static ArrayList<Integer> userNumber;
+
+    public User() {
+        userNumber = new ArrayList<>();
+    }
+
+    public void setUserNumber(ArrayList<Integer> imputNumber) {
+        ArrayList<Integer> user = new ArrayList<>();
+
+        String input = Console.readLine();
+        for (int i = 0; i < 3; i++) {
+            user.add(input.charAt(i) - '0');  //입력받은 문자열을 하나씩 끊어서 리스트에 저장
+        }
+
+        this.userNumber = imputNumber;
+    }
+}
