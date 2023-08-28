151e5900df9b3fb2dc8ed88450516d55027a1a39
Yeonwoo <yoanwoo0227@gmail.com>
2023-08-28 21:44:02 +0900

    Fix : User 숫자 setter에서 예외 처리 추가
    유저가 입력한 숫자가 3자리 숫자가 아니면 예외처리를 해준다.

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
-    private static ArrayList<Integer> userNumber;
+    private ArrayList<Integer> userNumber;
 
     public User() {
         userNumber = new ArrayList<>();
@@ -15,15 +15,14 @@ public class User {
         ArrayList<Integer> user = new ArrayList<>();
 
         String input = Console.readLine();
+        if (input.length() != Constants.NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
         for (int i = 0; i < Constants.NUMBER_LENGTH; i++) {
             user.add(input.charAt(i) - '0');  //입력받은 문자열을 하나씩 끊어서 리스트에 저장
         }
 
         this.userNumber = user;
-    }
-
-    public ArrayList<Integer> getUserNumber() {
-        return userNumber;
     }
 
     public int getUserNumberIdx(int idx) {
