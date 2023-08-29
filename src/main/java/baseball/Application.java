1509537da1bfeacff8c1de1fd3611f5944cffe7a
Yeonwoo <yoanwoo0227@gmail.com>
2023-08-29 21:13:35 +0900

    Fix : main 및 Game 클래스 변경
    main에는 Game 객체 생성 및 playGame() 호출만 있도록.
    게임 재시작 여부는 playGame 메소드 안에 구현하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,18 +7,7 @@ public class Application {
         // TODO: 프로그램 구현
         Game game = new Game();
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (true) {
-            game.playGame();
-            if (reStartOrEnd()) {
-                break;
-            }
-        }
+        game.playGame();
 
-    }
-
-    public static boolean reStartOrEnd() {
-        int inputNumber = Integer.parseInt(Console.readLine());
-
-        return inputNumber == Constants.END_NUMBER;
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 
     private static Computer computerNumbers = new Computer();
@@ -9,23 +11,22 @@ public class Game {
 
     public void playGame() {
         computerNumbers.setComputerNumber();
-
         while (true) {
             strike = 0;
             ball = 0;
-
             userNumbers.setUserNumber();
             getStrikeAndBall();
             printResult();
-
-            if (strike == 3) {
+            if (strike == Constants.NUMBER_LENGTH) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
         }
+        reStart();
     }
 
-    public static void getStrikeAndBall() { //스트라이크인지 체크하고, 아니면 볼인지 체크한다.
+
+    public void getStrikeAndBall() { //스트라이크인지 체크하고, 아니면 볼인지 체크한다.
         for (int i = 0; i < Constants.NUMBER_LENGTH; i++) {
             //컴퓨터와 사용자의 3자리 숫자를 각 자릿수 별로 같은 자리에 같은 숫자가 있는지 확인하고, 있으면 스트라이크 없으면 볼 체크 함수를 불러온다.
             if (computerNumbers.getComputerNumberIdx(i) == userNumbers.getUserNumberIdx(i)) {
@@ -36,7 +37,7 @@ public class Game {
         }
     }
 
-    public static void getBall(int idx) {
+    public void getBall(int idx) {
         //컴퓨터 숫자 리스트에 매개변수로 받은 인덱스번째에 있는 사용자의 숫자가 포함되어 있으면 볼
         if (computerNumbers.getComputerNumber().contains(userNumbers.getUserNumberIdx(idx))) {
             ball++;
@@ -54,5 +55,12 @@ public class Game {
             System.out.printf("%d볼 %d스트라이크\n", ball, strike);
         }
     }
+
+    public static boolean reStart() {
+        int inputNumber = Integer.parseInt(Console.readLine());
+
+        return inputNumber != Constants.END_NUMBER;
+    }
 }
+
 
